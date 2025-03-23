f0954da8a6801dbfb88561b334c18d7094de1aae
BibinBabu755 <bibinbabuayur@gmail.com>
2025-03-23 11:18:56 +0530

    ok ok chumma

--- script.js
+++
@@ -1,4 +0,0 @@
-// Add an event listener to the "Get Started" button
-document.getElementById('getStartedButton').addEventListener('click', function() {
-    alert('Thank you for choosing us!');
-});
